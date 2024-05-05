a2e5e5545c96518bea4341354780e4821cf44b92
Evgeniy <krasnoperoveg@mail.ru>
2024-05-05 19:31:25 +0200

    add models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    image = models.ImageField()
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
