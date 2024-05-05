39092f2216392930ea97eebb5493c368a2bbe7ea
Evgeniy <krasnoperoveg@mail.ru>
2024-05-05 19:34:15 +0200

    add admin settings

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import Tag, News
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
