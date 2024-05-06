8420d7ab662bb7d10bd253fc95e8b207beeb9817
Evgeniy <krasnoperoveg@mail.ru>
2024-05-06 11:41:18 +0200

    add search for tag

--- news/views.py
+++ news/views.py
@@ -7,6 +7,13 @@ class AllNewsView(ListView):
     model = News
     template_name = 'all_news.html'
 
+    def get_queryset(self):
+        tag = self.request.GET.get("tag")
+        if tag:
+            news = self.model.objects.filter(tags__name__in=[tag, ])
+            return news
+        return super().get_queryset()
+
 
 class NewsDetailView(DetailView):
     model = News
