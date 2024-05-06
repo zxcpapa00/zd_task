4144c39f6198994721ed714e2e665036986d6a5f
Evgeniy <krasnoperoveg@mail.ru>
2024-05-06 10:48:00 +0200

    news detail

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from news.views import AllNewsView
+from news.views import AllNewsView, NewsDetailView
 
 urlpatterns = [
-    path('', AllNewsView.as_view(), name='news')
+    path('', AllNewsView.as_view(), name='news'),
+    path('<int:pk>/', NewsDetailView.as_view(), name='news-detail')
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from news.models import News
 
@@ -6,3 +6,8 @@ from news.models import News
 class AllNewsView(ListView):
     model = News
     template_name = 'all_news.html'
+
+
+class NewsDetailView(DetailView):
+    model = News
+    template_name = 'news_detail.html'
